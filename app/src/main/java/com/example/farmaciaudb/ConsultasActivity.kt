3200df2b78f1eaada101495124d3814938cc402e
SiletGer <josemiguelchavezmartinez47@gmail.com>
2024-04-12 13:40:34 -0600

    version 1

---
+++ app/src/main/java/com/example/farmaciaudb/ConsultasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.farmaciaudb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConsultasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_consultas)
+    }
+}
